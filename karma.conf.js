8d77223f651d9000d2b9e48a1cf6b34fa41f58fb
sun-sunny <1135621018@qq.com>
2019-08-04 00:03:39 +0800

    travis.10

--- karma.conf.js
+++ karma.conf.js
@@ -74,8 +74,4 @@ module.exports = function(config) {
         // how many browser should be started simultaneous
         concurrency: Infinity
     })
-
-    if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_without_security'];
-    }
 }
