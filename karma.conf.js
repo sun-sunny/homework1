b5681515ca92998ddf20915b114ec6170ddf0701
sun-sunny <1135621018@qq.com>
2019-08-03 23:19:27 +0800

    travis.3

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,8 @@ module.exports = function(config) {
         customLaunchers: {
             Chrome_without_security: {
                 base: 'Chrome',
-                flags: ['--disable-web-security']
+                flags: ['--no-sandbox']
+                    // flags: ['--disable-web-security']
             }
         },
         // browsers: ['Chrome'],
